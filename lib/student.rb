e20c3df9ee870fd95a3310ef9ad1e887aae71fd7
Kayla Luebbert <luebbertkayla@gmail.com>
2021-05-17 06:12:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -62,4 +62,12 @@ class Student
     DB[:conn].execute(sql)
   end
   
-  def self.
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT * 
+      FROM students 
+      WHERE grade = 9 
+     SQL
+     
+     DB[:conn].execute(sql).map do |row|
+       self.new_from_db(row)
