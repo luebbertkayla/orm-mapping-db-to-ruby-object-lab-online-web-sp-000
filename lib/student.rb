ea7dae8409487060817ee8720407dcde1ca0b773
Kayla Luebbert <luebbertkayla@gmail.com>
2021-05-17 06:03:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,10 +3,7 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
-    sql = <<-SQL
-      SELECT * 
-      FROM students 
-     SQL
+   
   end
 
   def self.all
